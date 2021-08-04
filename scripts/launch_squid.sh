32c37bf6054c2e6f0e7e707d484b2a85216b7fbe
dciangot <dciangot@cern.ch>
2021-08-04 14:49:50 +0200

    Update launch_squid.sh

--- scripts/launch_squid.sh
+++ scripts/launch_squid.sh
@@ -6,11 +6,12 @@ while true; do
 
   string=`/etc/init.d/frontier-squid status`
 
-  if [[ $string != *"Frontier Squid running\n"* ]]; then
+  if [[ $string != *"Frontier Squid running"* ]]; then
     echo "not running"
     break
   fi
 
+sleep 30
 done
 
 echo "Exiting."
